3687c6e6452c4086b9be1c6c6f6a115504cd7759
Robbe <robbevaes95@gmail.com>
2023-09-22 17:33:31 +0200

    Added top white bar

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,8 @@
 <template>
   <div class=" h-screen w-screen">
     <div class="absolute top-0 w-full">
-      <div class="flex w-full items-center justify-center py-4">
-        <img src="@/assets/ag.png" class="w-80 rounded bg-white p-4">
+      <div class="flex w-full items-center justify-center  bg-white py-4">
+        <img src="@/assets/ag.png" class="w-80 ">
       </div>
     </div>
     <iframe class="h-full w-full" title="car" frameborder="0" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src="https://sketchfab.com/models/af91c35dd1ee4afeacc89134df603fa2/embed?autostart=1" />
